397152a08a28b365badc1a60147316bc0d0d8e6d
Mike-920-dev <65869511+Mike920-dev@users.noreply.github.com>
2021-09-17 22:14:49 +0200

    update script.py(include new files)

--- script.py
+++ script.py
@@ -1,26 +1,8 @@
 import hikari
-import mysql.connector
+from snippet import display_chosen_snippet
+from list_snippets import list_language_snippets
+from search import search_snippet
 from dotenv import dotenv_values
-
-def connect_to_database(host, user, passwd, port, database, snippet_name, language):
-    # connection
-    myDb = mysql.connector.connect(host=host, user=user, passwd=passwd, port=port, database=database)
-    cursor = myDb.cursor()
-
-    query = (
-        f"""
-        SELECT snippet.name, code FROM snippet JOIN language ON snippet.language_id=language.id WHERE snippet.name LIKE '{snippet_name}' AND language.name LIKE '{language}';
-        """
-    )
-
-    # we execute the operation stored in the query variable
-    cursor.execute(query)
-
-    for (name, code) in cursor:
-        return f'{code}'
-
-    cursor.close()
-    myDb.close()
 
 # config .env .env.local
 config = {
@@ -42,16 +24,19 @@ async def display_snippet(event: hikari.GuildMessageCreateEvent):
     # user's message
     msg = event.content.split()[2:]
 
-    # snippet_name and chosen programming language
-    snippet_name = msg[0]
-    language = msg[1]
+    if event.content.startswith("$ snippet") and len(msg) == 2:
+        snippet_name = msg[0]
+        snippet_language = msg[1]
 
-    # respond with the snippet
-    if event.content.startswith("$ snippet"):
-        connection = connect_to_database(config['HOST'], config['USER'], config['PASSWD'], config['PORT'], config['DATABASE'], snippet_name, language)
-        if  connection == None:
-            await event.message.respond(f'Snippet not found!')
-        else:
-            await event.message.respond(f"""```{language}\n{connection}\n```""")
+        await event.message.respond(display_chosen_snippet(snippet_name, snippet_language))
+    elif event.content.startswith("$ list") and len(msg) == 1:
+        listed_language = msg[0]
+        await event.message.respond(list_language_snippets(listed_language))
+    elif event.content.startswith("$ search") and len(msg) == 1:
+        listed_snippet = msg[0]
+        await event.message.respond(search_snippet(listed_snippet))
+    else:
+        if event.content.startswith("$"):
+            await event.message.respond("Unknown command :-(")
 
 bot.run()
